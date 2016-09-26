77a4920943ebe45c9424bca0ab0894a9c0bba90b
Ben Foxall <ben@pusher.com>
2016-09-26 15:03:47 +0100

    Initial app with auth and redis setup

---
+++ server.js
@@ -0,0 +1,70 @@
+require('dotenv').config()
+
+const express = require('express')
+const passport = require('passport')
+const Redis = require('ioredis')
+const session = require('express-session')
+const RedisStore = require('connect-redis')(session)
+
+const app = express()
+const redis = new Redis(process.env.REDIS_URL)
+
+app.use(require('cookie-parser')())
+app.use(require('body-parser').urlencoded({ extended: true }))
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true,
+  store: new RedisStore({
+    client: redis
+  })
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.serializeUser(function(user, done) {
+  redis.set(`user:${user.id}`, user._raw, function(err, data){
+    done(err, user.id)
+  })
+})
+
+passport.deserializeUser(function(id, done) {
+  redis.get(`user:${id}`, function(err, data){
+    done(err, JSON.parse(data))
+  })
+})
+
+var GoogleStrategy = require('passport-google-oauth20').Strategy
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    hd: 'pusher.com'
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log('FOUND', profile)
+    cb(null, profile)
+  }
+))
+
+app.get('/login', passport.authenticate('google', { scope: ['profile', 'email'] }))
+
+app.get('/callback',
+  passport.authenticate('google'),
+  (req, res) => res.redirect('/')
+)
+
+app.get('/logout', function(req, res){
+  req.logout()
+  res.redirect('/')
+})
+
+
+app.set('view engine', 'pug')
+
+app.get('/', (req, res) => {
+  res.render('index', {user: req.user})
+})
+
+app.listen(3000 || process.env.PORT)
