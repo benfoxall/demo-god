97660ab18bf3ba1e7cf54e4f292cdc0018a8062b
Ben Foxall <ben@pusher.com>
2016-10-03 19:36:53 +0100

    First ever demo

---
+++ demos/broadcast/README.md
@@ -0,0 +1,13 @@
+REPLACE_THIS
+
+# Broadcast demo
+
+This is a basic demo to show how you can send out data from a central source
+
+
+### Giving a demo
+
+1. login
+2. Provide app details (can be faked)
+3. Download a demo
+4. Run it (by clicking)
---
+++ demos/broadcast/broadcast.js
@@ -0,0 +1,13 @@
+const Pusher = require('pusher')
+
+const pusher = new Pusher({
+  appId:   '~~PUSHER_APP_ID~~',
+  key:     '~~PUSHER_KEY~~',
+  secret:  '~~PUSHER_SECRET~~',
+  cluster: '~~PUSHER_CLUSTER~~',
+  encrypted: true
+})
+
+pusher.trigger('~~DEMO_KEY~~_broadcast', 'display-text', {
+  text: 'Hello world ' + Math.random()
+})
